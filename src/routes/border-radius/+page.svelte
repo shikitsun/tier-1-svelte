f4def5649db6011a7c4d554f3769fa4249e1539c
Zealxl <tempest0zeta@gmail.com>
2023-12-20 01:06:48 +0300

    switch between simple / full control of borders

--- src/routes/border-radius/+page.svelte
+++ src/routes/border-radius/+page.svelte
@@ -1,14 +1,16 @@
 <script>
-  import { createBorder, formString, parseString } from '$lib/border-radius';
+  import { BASE_BORDER, BASE_BORDER2, formString, parseString } from '$lib/border-radius';
   import { onMount } from 'svelte';
   import { browser } from '$app/environment';
   import BorderMovable from '$lib/components/BorderMovable.svelte';
   import { debounce } from '$lib';
 
-  let border = parseString('0.100.100.0.0-100.0.0.100');
+  let border = parseString(BASE_BORDER);
   let mounted = false;
   let overlayWidth = 0;
   let overlayHeight = 0;
+  let fullControl = false;
+
   onMount(() => {
     const { hash } = window.location;
     mounted = true;
@@ -20,7 +22,7 @@
     /** @param {import('$lib/border-radius').Border} border */ (border) => {
       // no need use localStorage, because this is not production
       // but may replaced with it as well if needed
-      window.location.hash = formString(border);
+      // window.location.hash = formString(border);
     },
     400
   );
@@ -30,59 +32,161 @@
   $: if (browser && mounted) {
     update(border);
   }
+
+  $: if (!fullControl) {
+    // if switched from fullControl -> no fullControl
+    border.topRight.main = 100 - border.topLeft.main;
+    border.bottomRight.primary = 100 - border.topRight.primary;
+    border.bottomLeft.primary = 100 - border.topLeft.primary;
+    border.bottomRight.main = 100 - border.bottomLeft.main;
+  }
+
+  /**
+   *
+   * @param {Event} ev
+   */
+  function checkBorder(ev) {
+    const { checked } = /** @type {HTMLInputElement} */ (ev.target);
+    const borderStr = formString(border);
+    console.log(checked, borderStr, borderStr === BASE_BORDER, borderStr === BASE_BORDER2);
+    if (checked && borderStr === BASE_BORDER) border = parseString(BASE_BORDER2);
+    else if (!checked && borderStr === BASE_BORDER2) border = parseString(BASE_BORDER);
+  }
 </script>
 
 <main>
   <div class="app">
     <div class="fig" style:border-radius={borderRadius} />
     <div class="overlay" bind:clientWidth={overlayWidth} bind:clientHeight={overlayHeight}>
-      <BorderMovable
-        bind:value={border.topLeft.main}
-        direction="horizontal"
-        length={overlayWidth}
-        on:change={({ detail }) => {
-          border.topRight.main = 100 - detail;
-        }}
-      />
+      {#if fullControl}
+        <BorderMovable
+          bind:value={border.topLeft.main}
+          direction="horizontal"
+          length={overlayWidth}
+        />
 
-      <BorderMovable
-        bind:value={border.topRight.primary}
-        direction="vertical"
-        length={overlayHeight}
-        on:change={({ detail }) => {
-          border.bottomRight.primary = 100 - detail;
-        }}
-        --baseX={overlayWidth + 'px'}
-        --offsetX={5 + 'px'}
-      />
+        <BorderMovable
+          bind:value={border.topLeft.primary}
+          direction="vertical"
+          length={overlayHeight}
+        />
 
-      <BorderMovable
-        bind:value={border.topLeft.primary}
-        direction="vertical"
-        length={overlayHeight}
-        on:change={({ detail }) => {
-          border.bottomLeft.primary = 100 - detail;
-        }}
-      />
+        <BorderMovable
+          bind:value={border.topRight.main}
+          direction="horizontal"
+          length={overlayWidth}
+          reverse
+          --baseX={overlayWidth + 'px'}
+        />
 
-      <BorderMovable
-        bind:value={border.bottomLeft.main}
-        direction="horizontal"
-        length={overlayWidth}
-        on:change={({ detail }) => {
-          border.bottomRight.main = 100 - detail;
-        }}
-        --baseY={overlayHeight + 'px'}
-        --offsetX={5 + 'px'}
-      />
+        <BorderMovable
+          bind:value={border.topRight.primary}
+          direction="vertical"
+          length={overlayHeight}
+          --baseX={overlayWidth + 'px'}
+          --offsetX={'5px'}
+        />
+
+        <BorderMovable
+          bind:value={border.bottomRight.main}
+          direction="horizontal"
+          length={overlayWidth}
+          reverse
+          --baseX={overlayWidth + 'px'}
+          --baseY={overlayHeight + 'px'}
+        />
+
+        <BorderMovable
+          bind:value={border.bottomRight.primary}
+          direction="vertical"
+          length={overlayHeight}
+          reverse
+          --baseX={overlayWidth + 'px'}
+          --baseY={overlayHeight + 'px'}
+        />
+
+        <BorderMovable
+          bind:value={border.bottomLeft.main}
+          direction="horizontal"
+          length={overlayWidth}
+          --baseY={overlayHeight + 'px'}
+        />
+
+        <BorderMovable
+          bind:value={border.bottomLeft.primary}
+          direction="vertical"
+          length={overlayHeight}
+          reverse
+          --baseY={overlayHeight + 'px'}
+        />
+      {:else}
+        <BorderMovable
+          bind:value={border.topLeft.main}
+          direction="horizontal"
+          length={overlayWidth}
+          on:change={({ detail }) => {
+            border.topRight.main = 100 - detail;
+          }}
+        />
+
+        <BorderMovable
+          bind:value={border.topRight.primary}
+          direction="vertical"
+          length={overlayHeight}
+          on:change={({ detail }) => {
+            border.bottomRight.primary = 100 - detail;
+          }}
+          --baseX={overlayWidth + 'px'}
+          --offsetX={5 + 'px'}
+        />
+
+        <BorderMovable
+          bind:value={border.topLeft.primary}
+          direction="vertical"
+          length={overlayHeight}
+          on:change={({ detail }) => {
+            border.bottomLeft.primary = 100 - detail;
+          }}
+        />
+
+        <BorderMovable
+          bind:value={border.bottomLeft.main}
+          direction="horizontal"
+          length={overlayWidth}
+          on:change={({ detail }) => {
+            border.bottomRight.main = 100 - detail;
+          }}
+          --baseY={overlayHeight + 'px'}
+          --offsetX={5 + 'px'}
+        />
+      {/if}
     </div>
   </div>
 
-  <div class="style">
-    <div class="label">border-radius:</div>
-    <div class="input">
-      <code class="code">{borderRadius}</code>
-      <button class="btn">Copy</button>
+  <div class="footer">
+    <div class="row">
+      <div class="input">
+        <input
+          value={borderRadius}
+          class="code"
+          disabled
+          readonly
+          style:width={`calc(${borderRadius.length + 'ch'} + var(--p) * 2)`}
+          id="borderRadiusOut"
+        />
+        <button class="btn">Copy</button>
+      </div>
+    </div>
+
+    <div class="row">
+      <label class="label" for="fullControl">Full Control</label>
+      <input
+        bind:checked={fullControl}
+        class="switch"
+        id="fullControl"
+        type="checkbox"
+        on:change={checkBorder}
+      />
     </div>
     <!-- custom size here -->
   </div>
@@ -125,9 +229,15 @@
     height: var(--height);
   }
 
-  .style {
-    display: grid;
-    grid-template-areas: 'label input' 'label input';
+  .footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  .row {
+    display: flex;
     align-items: center;
     gap: 0.5rem;
   }
@@ -139,9 +249,8 @@
   }
 
   .input {
-    grid-area: 'input';
     display: flex;
-    align-items: center;
+    align-items: stretch;
     font-size: 1rem;
     line-height: 1;
     --p: 6px;
@@ -155,6 +264,10 @@
     background-color: #eee;
     color: #222;
     user-select: none;
+    font-family: 'Courier New', Courier, monospace;
+    border: none;
+    font-size: 1rem;
+    line-height: 1;
   }
 
   .btn {
@@ -173,6 +286,41 @@
     transition-property: background-color;
     transition-duration: 150ms;
     color: white;
+    text-transform: uppercase;
+    font-weight: 600;
+    cursor: copy;
+  }
+
+  .switch {
+    --val: 0;
+    --bg: #18e8;
+    --width: 40px;
+    --height: 20px;
+    all: unset;
+    position: relative;
+    width: var(--width);
+    height: var(--height);
+    border-radius: 8px;
+    background-color: white;
+  }
+
+  .switch::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    display: block;
+    width: calc(var(--width) / 2);
+    height: var(--height);
+    background-color: var(--bg);
+    border-radius: 8px;
+    transform: translateX(calc(var(--val) * var(--width) / 2));
+    transition-property: transform background-color;
+    transition-duration: 150ms;
+  }
+
+  .switch:checked {
+    --val: 1;
+    --bg: #18ec;
   }
 
   .btn:hover {
