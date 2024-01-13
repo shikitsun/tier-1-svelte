20834212115afd56ae7ce601b1abe6a0fc93115f
shikitsun <tempest0zeta@gmail.com>
2024-01-13 15:51:06 +0300

    init cause effect & move global styles to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,16 @@
 </script>
 
 <slot />
+
+<style>
+  :global(body) {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    background-color: #121212;
+    color: #edede9ee;
+    min-width: 100vw;
+    min-height: 100vh;
+    padding: 0;
+  }
+</style>
---
+++ src/routes/cause-effect/+page.svelte
@@ -0,0 +1,5 @@
+<script></script>
+
+<main />
+
+<style></style>
